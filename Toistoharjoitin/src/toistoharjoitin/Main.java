34da8cc956c370f04ac7f6463cc3ab2aed8529d3
Paavo P O Ojala <ppoojala@dhp-27.cs.helsinki.fi>
2012-09-04 11:09:34 +0300

    jotain

---
+++ Toistoharjoitin/src/toistoharjoitin/Main.java
@@ -0,0 +1,19 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package toistoharjoitin;
+
+/**
+ *
+ * @author ppoojala
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+}
